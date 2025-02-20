2c8c915c5b4c6b640924928941c5f09182fbe3b4
Vasil Kostadinov <vasil.kostadinov@3web-studio.com>
2025-02-20 15:55:26 +0200

    added new utilitites

--- src/imap/index.ts
+++ src/imap/index.ts
@@ -2,7 +2,9 @@ import IMAPClientUtils from './IMAPClientUtils'
 import getQuota from './actions/getQuota'
 import searchForMessages from './actions/searchForMessages'
 import sendMessage from './actions/sendMessage'
+import getTransport from './actions/getTransport'
+import syncMailbox from './actions/syncMailbox'
 const createIMAPClient = IMAPClientUtils.createClient
 
 
-export { createIMAPClient, getQuota, searchForMessages, sendMessage }
+export { createIMAPClient, getQuota, searchForMessages, sendMessage, getTransport, syncMailbox }
