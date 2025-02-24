5c2fe601b7c4e0ae504b5149e13349ce3e1b84d7
Vasil Kostadinov <vasil.kostadinov@3web-studio.com>
2025-02-24 10:48:17 +0200

    search messages in specific mailboxes

--- src/imap/actions/searchForMessages.ts
+++ src/imap/actions/searchForMessages.ts
@@ -11,7 +11,8 @@ export default async function (
     order: "asc" | "desc" = "desc"
 ): Promise<IMailResult> {
     return await IMAPClientUtils.executeIMAPCommand(client, async (): Promise<IMailResult> => {
-
+        if (searchFor.mailbox) await client.imap.mailboxOpen(searchFor.mailbox);
+        
         let query: any = {};
         if (searchFor.messageId) query = {
             ...query,
--- src/imap/index.ts
+++ src/imap/index.ts
@@ -4,7 +4,8 @@ import searchForMessages from './actions/searchForMessages'
 import sendMessage from './actions/sendMessage'
 import getTransport from './actions/getTransport'
 import syncMailbox from './actions/syncMailbox'
+import getMailboxes from './actions/getMailboxes'
 const createIMAPClient = IMAPClientUtils.createClient
 
 
-export { createIMAPClient, getQuota, searchForMessages, sendMessage, getTransport, syncMailbox }
+export { createIMAPClient, getQuota, searchForMessages, sendMessage, getTransport, syncMailbox, getMailboxes }
